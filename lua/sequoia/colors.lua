b233087252a6ec975ede2be81a40d672d19d6e89
Hiroya-W <hyuyukun@gmail.com>
2022-07-26 13:55:16 +0900

    :sparkles: Defined color palette for sequoia-moonlight

---
+++ lua/sequoia/colors.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.setup()
+    local colors = {}
+
+    colors = {
+        base = "#0F1014",
+        text = "#868690",
+        blue = "#8eb6f5",
+        purple = "#c58fff",
+        pink = "#f58ee0",
+        orange = "#ffbb88",
+        gray = "#9898a6",
+        white = "#fdfdfe",
+    }
+
+    return colors
+end
+
+return M
