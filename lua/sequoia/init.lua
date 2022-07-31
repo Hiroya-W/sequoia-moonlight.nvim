1eadb81234b5d8b63d8ccf8b9ad971a3b72a7d66
Hiroya-W <hyuyukun@gmail.com>
2022-07-31 17:40:01 +0900

    :+1: Add terminal colors

--- lua/sequoia/init.lua
+++ lua/sequoia/init.lua
@@ -1,4 +1,5 @@
 local theme = require("sequoia.theme").setup()
+local sequoia_colors = require("sequoia.colors").setup()
 local M = {}
 
 local function highlight(group, color)
@@ -16,6 +17,38 @@ local function highlight(group, color)
     end
 end
 
+local function terminal()
+    vim.g.terminal_color_0 = sequoia_colors.bg
+    vim.g.terminal_color_8 = sequoia_colors.bg
+
+    vim.g.terminal_color_7 = sequoia_colors.fg
+    vim.g.terminal_color_15 = sequoia_colors.fg
+
+    -- red
+    vim.g.terminal_color_1 = sequoia_colors.pink
+    vim.g.terminal_color_9 = sequoia_colors.pink
+
+    -- green
+    vim.g.terminal_color_2 = sequoia_colors.blue
+    vim.g.terminal_color_10 = sequoia_colors.blue
+
+    -- yellow
+    vim.g.terminal_color_3 = sequoia_colors.gray
+    vim.g.terminal_color_11 = sequoia_colors.gray
+
+    -- blue
+    vim.g.terminal_color_3 = sequoia_colors.purple
+    vim.g.terminal_color_11 = sequoia_colors.purple
+
+    -- magenta
+    vim.g.terminal_color_4 = sequoia_colors.white
+    vim.g.terminal_color_12 = sequoia_colors.white
+
+    -- cyan
+    vim.g.terminal_color_4 = sequoia_colors.orange
+    vim.g.terminal_color_12 = sequoia_colors.orange
+end
+
 function M.colorscheme()
     if vim.g.color_name then
         vim.cmd("hightlight clear")
@@ -26,6 +59,8 @@ function M.colorscheme()
     for group, color in pairs(theme.base) do
         highlight(group, color)
     end
+
+    terminal()
 end
 
 return M
